d7c9347dde7f3e749fb71c86461cfa25a2c9dfbe
Humberto Avila <havilao@toluca.tecnm.mx>
2021-04-21 09:27:37 -0500

    Added media queries
    - Added night mode toggle

---
+++ js/main.js
@@ -0,0 +1,25 @@
+document.getElementById("toggleDark").addEventListener("click", toggleDark);
+let enabled = false;
+let counter = 0;
+
+function toggleDark(){
+    counter++;
+    // This is a pretty horrible implementation, but it works
+    if (counter >= 2) counter = 0;
+    else if (counter == 1){
+        enabled = !enabled;
+        let r = document.querySelector(':root');
+        let rs = getComputedStyle(r);
+
+        if (enabled){
+            r.style.setProperty('--background', '#fff');
+            r.style.setProperty('--foreground', '#111');
+            r.style.setProperty('--foreground-shine', '#111b0');
+        } else {
+            r.style.setProperty('--background', '#383838');
+            r.style.setProperty('--foreground', '#fff');
+            r.style.setProperty('--foreground-shine', '#ffffffb0');
+        }
+        console.log(("The new value of --bg is: " + rs.getPropertyValue('--background')));
+    }
+}
