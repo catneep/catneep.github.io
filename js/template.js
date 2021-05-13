d2b5e0ece50dc7877dc78533a387fa3ace91cfcb
Humberto Avila <havilao@toluca.tecnm.mx>
2021-05-13 14:35:56 -0500

    Enabled Project Display
    - Disabled parallax effect (TODO)
    - template.js can now be called as a function
    - Added Projects styling to styles.css

--- js/template.js
+++ js/template.js
@@ -1,19 +1,23 @@
-if ('content' in document.createElement('template')) {
-    console.log('Template element is supported');
+let drawn = false;
+
+function drawTemplates(){
+    if ('content' in document.createElement('template')) {
+        console.log('Template element is supported');
+        
+        //Contenedor para elementos de template
+        const container = document.querySelector("#project-list");
+        //Elemento a clonar
+        const template = document.querySelector('#project-card-template');
     
-    //Contenedor para elementos de template
-    const container = document.querySelector("#project-list");
-    //Elemento a clonar
-    const template = document.querySelector('#template');
-
-    // Clone the new row and insert it into the table
-    for (i = 0; i < 7; i++){
-        let clone = template.content.cloneNode(true);
-        let p = clone.querySelectorAll('h3');
-        p[0].textContent = `idolazo ${i + 1}`
-        // p[1].textContent = `element: ${i + 1}`;
-
-        container.appendChild(clone);
-    }
-
-} else console.log('Template element is NOT supported');
+        // Clone the new row and insert it into the table
+        for (i = 0; i < 7; i++){
+            let clone = template.content.cloneNode(true);
+            let p = clone.querySelectorAll('h3');
+            p[0].textContent = `idolazo ${i + 1}`
+            // p[1].textContent = `element: ${i + 1}`;
+    
+            container.appendChild(clone);
+        }
+        drawn = true;
+    } else console.log('Template element is NOT supported');
+}
